a665156589b2e3bbe1ff9ef8e340152f217fffa0
Gavriliu Andrei <andreig7651@gmail.com>
2024-04-10 16:43:01 +0300

    B2

---
+++ app/src/main/java/com/example/colocviu1_1mainactivity/Constants.java
@@ -0,0 +1,6 @@
+package com.example.colocviu1_1mainactivity;
+
+public class Constants {
+    final public static String NUMBER_OF_CLICKS= "numberOfClicks";
+    final public static String INPUT_HISTORY = "inputHistory";
+}
--- app/src/main/java/com/example/colocviu1_1mainactivity/MainActivity.java
+++ app/src/main/java/com/example/colocviu1_1mainactivity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String result = "";
 
+    private Integer numberOfPoints = 0;
+
     private final ButtonClickListener buttonClickListener = new ButtonClickListener();
     private class ButtonClickListener implements View.OnClickListener {
 
@@ -27,23 +30,52 @@ public class MainActivity extends AppCompatActivity {
 
             if (view.getId() == R.id.northButton) {
                 result += "North, ";
+                numberOfPoints++;
                 inputHistory.setText(result);
             }
 
             if (view.getId() == R.id.southButton) {
                 result += "South, ";
+                numberOfPoints++;
                 inputHistory.setText(result);
             }
 
             if (view.getId() == R.id.eastButton) {
                 result += "East, ";
+                numberOfPoints++;
                 inputHistory.setText(result);
             }
 
             if (view.getId() == R.id.westButton) {
                 result += "West, ";
+                numberOfPoints++;
                 inputHistory.setText(result);
             }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(Constants.NUMBER_OF_CLICKS, numberOfPoints.toString());
+        savedInstanceState.putString(Constants.INPUT_HISTORY, inputHistory.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(Constants.INPUT_HISTORY)) {
+            inputHistory.setText(savedInstanceState.getString(Constants.INPUT_HISTORY));
+        } else {
+            inputHistory.setText("");
+        }
+        if (savedInstanceState.containsKey(Constants.NUMBER_OF_CLICKS)) {
+            Toast.makeText(this, "Button clicked " +
+                    savedInstanceState.getString(Constants.NUMBER_OF_CLICKS) +
+                    " times", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Button clicked " +
+                            savedInstanceState.getString(Constants.NUMBER_OF_CLICKS) +
+                    " times", Toast.LENGTH_LONG).show();
         }
     }
 
